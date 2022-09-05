27571a5c5bb33ccd00d177ec28c13755651e2d26
almarglenmarcelo <ag.marcelo28@gmail.com>
2022-09-05 22:05:21 +0800

    Configured CommandLineRunner

--- src/main/java/todo/app/Application.java
+++ src/main/java/todo/app/Application.java
@@ -1,17 +1,35 @@
 package todo.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Application{
+public class Application implements CommandLineRunner{
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-	
+	private static Logger LOG = LoggerFactory
+		      .getLogger(Application.class);
+
+		    public static void main(String[] args) {
+		        LOG.info("STARTING THE APPLICATION");
+		        SpringApplication.run(Application.class, args);
+		        LOG.info("APPLICATION FINISHED");
+		    }
+		 
+		    @Override
+		    public void run(String... args) {
+		        LOG.info("EXECUTING : command line runner");
+		 
+		        for (int i = 0; i < args.length; ++i) {
+		            LOG.info("args[{}]: {}", i, args[i]);
+		        }
+		    }
+		    
+		    
+		    
 	
 
 }
