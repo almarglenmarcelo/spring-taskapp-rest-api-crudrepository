92ef32eb1b1b15526ef772e65f7c4f2d9f8eb2bd
almarglenmarcelo <ag.marcelo28@gmail.com>
2022-09-07 00:55:15 +0800

    Configured the redirect link

--- src/main/java/todo/app/SpringFoxConfig.java
+++ src/main/java/todo/app/SpringFoxConfig.java
@@ -3,12 +3,16 @@ package todo.app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() { 
@@ -18,4 +22,13 @@ public class SpringFoxConfig {
           .paths(PathSelectors.any())                          
           .build();                                           
     }
+    
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("JavaInUse API")
+				.description("JavaInUse API reference for developers")
+				.termsOfServiceUrl("http://javainuse.com")
+				.license("JavaInUse License")
+				.licenseUrl("javainuse@gmail.com").version("1.0").build();
+	}
+    
 }
